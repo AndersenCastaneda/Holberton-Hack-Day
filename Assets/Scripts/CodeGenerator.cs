f511dcd880b26cc42f66c839273fa3ad0e22863e
AndersenCastaneda <34457834+AndersenCastaneda@users.noreply.github.com>
2020-09-18 12:16:44 -0500

    Add Directories

---
+++ Assets/Scripts/CodeGenerator.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public static class CodeGenerator
+{
+	private static int[] arrayCode = new int[4];
+	private static int length = 4;
+
+	//MasterCode Generator
+	public static int[] MasterCode()
+	{
+		for (int i = 0; i < length; i++)
+			arrayCode[i] = Random.Range(0, 6);
+
+		return arrayCode;
+	}
+
+	//Check if any position/'color' (value info) is Correct
+	public static bool IsCodeCorrect(int[] userCode)
+	{
+		for (int i = 0; i < length; i++)
+		{
+			for (int j = 0; j < length; j++)
+			{
+				// if (arrayCode[i] == userCode[j])
+			}
+			if (arrayCode[i] != userCode[i])
+				return false;
+		}
+		return true;
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public int[] masterCode = new int[4];
+
+	private void Awake()
+	{
+		masterCode = CodeGenerator.MasterCode();
+	}
+}
